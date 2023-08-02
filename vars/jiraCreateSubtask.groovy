163cdf5a35fb88a5c7a1319e71b8192bd46cff1a
sayanhazra2001 <82613453+sayanhazra2001@users.noreply.github.com>
2023-08-02 21:17:50 +0530

    Create jiraCreateSubtask.groovy

---
+++ vars/jiraCreateSubtask.groovy
@@ -0,0 +1,12 @@
+def createSubtask(Map config = [:]) {
+    def rawBody = libraryResource 'com/jenkins/api/jira/createsubtask.json'
+    def binding = [
+        key: "${config.key}",
+        parentKey: "${config.parentKey}",
+        summary: "${config.summary}",
+        description: "${config.description}",
+        issueTypeName: "${config.issueTypeName}"
+    ]
+    def render = renderTemplate(rawBody, binding)
+    sh('curl -D- -u $JIRA_CREDENTIALS -X POST --data "' + render + '" -H "Content-Type: application/json" $JIRA_URL/rest/api/2/issue')
+}
