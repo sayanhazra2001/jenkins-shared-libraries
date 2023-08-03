20d6505c7c617484bbd97ee753d44d9dac248be0
sayanhazra2001 <82613453+sayanhazra2001@users.noreply.github.com>
2023-08-03 22:44:59 +0530

    Update jiraUpdateStatus.groovy

--- vars/jiraUpdateStatus.groovy
+++ vars/jiraUpdateStatus.groovy
@@ -2,8 +2,8 @@ def call(Map config=[:]) {
     def rawBody = libraryResource 'com/jenkins/api/jira/updateStatus.json'
     def binding = [
         key: "${config.key}",
-        id: "${config.id}"
+        transitionId: "${config.transitionId}"
     ]
     def render = renderTemplate(rawBody,binding)
-  sh('curl -D- -u $JIRA_CREDENTIALS -X POST --data "'+render+'" -H "Content-Type: application/json" $JIRA_URL/rest/api/2/issue/${key}/transitions')
+  sh('curl -D- -u $JIRA_CREDENTIALS -X POST --data '${render.replaceAll("'", "'\\\\''")}' -H "Content-Type: application/json" $JIRA_URL/rest/api/2/issue/${config.key}/transitions')
 }
