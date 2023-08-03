5d8091cdc5ec7e8a3573ef28a37287e9f471db8d
sayanhazra2001 <82613453+sayanhazra2001@users.noreply.github.com>
2023-08-03 21:01:05 +0530

    Update jiraUpdateStatus.groovy

--- vars/jiraUpdateStatus.groovy
+++ vars/jiraUpdateStatus.groovy
@@ -5,5 +5,5 @@ def call(Map config=[:]) {
         transitionId: "${config.transitionId}"
     ]
     def render = renderTemplate(rawBody,binding)
-  sh('curl -D- -u $JIRA_CREDENTIALS -X POST --data \"$render\" -H "Content-Type: application/json" $JIRA_URL/rest/api/2/issue/${issueKey}/transitions')
+  sh('curl -D- -u $JIRA_CREDENTIALS -X POST --data \"$render\" -H "Content-Type: application/json" $JIRA_URL/rest/api/2/issue/POC-22/transitions')
 }
