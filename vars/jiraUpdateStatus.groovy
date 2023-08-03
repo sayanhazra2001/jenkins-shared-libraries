e1b3000cc5af2367a45d8d69e21fff4a917c1c7d
sayanhazra2001 <82613453+sayanhazra2001@users.noreply.github.com>
2023-08-03 21:05:49 +0530

    Update jiraUpdateStatus.groovy

--- vars/jiraUpdateStatus.groovy
+++ vars/jiraUpdateStatus.groovy
@@ -1,7 +1,6 @@
 def call(Map config=[:]) {
     def rawBody = libraryResource 'com/jenkins/api/jira/updateStatus.json'
     def binding = [
-        issueKey: "${config.issueKey}",
         transitionId: "${config.transitionId}"
     ]
     def render = renderTemplate(rawBody,binding)
