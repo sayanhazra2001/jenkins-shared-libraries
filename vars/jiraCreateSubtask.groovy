63cb9988b91d919f9b8aa8b608763dcd2923d6b7
sayanhazra2001 <82613453+sayanhazra2001@users.noreply.github.com>
2023-08-04 19:36:31 +0530

    Update jiraCreateSubtask.groovy

--- vars/jiraCreateSubtask.groovy
+++ vars/jiraCreateSubtask.groovy
@@ -8,5 +8,8 @@ def call(Map config=[:]) {
         issueTypeName: "${config.issueTypeName}"
     ]
     def render = renderTemplate(rawBody,binding)
-  sh('curl -D- -u $JIRA_CREDENTIALS -X POST --data "'+render+'" -H "Content-Type: application/json" $JIRA_URL/rest/api/2/issue')
+    def response = sh('curl -D- -u $JIRA_CREDENTIALS -X POST --data "'+render+'" -H "Content-Type: application/json" $JIRA_URL/rest/api/2/issue')
+    def jsonResponse = readJSON text: response
+    def subtaskKey = jsonResponse.key
+    echo "Subtask Key: ${subtaskKey}"
 }
